5f0797bf3e658832700696b85d961db1d17875fe
Kevin Malakoff <kmalakoff@gmail.com>
2025-07-15 16:30:05 -0700

    Upgrade modules

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as BigIntStats } from './BigIntStats.ts';
-export { default as toBigIntStats } from './toBigIntStats.ts';
-export { default as toStats } from './toStats.ts';
-export * from './types.ts';
--- dist/esm/toStats.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Stats } from 'fs';
-import type { AnyStats } from './types.ts';
-export default function toStats(stats: AnyStats): Stats;
