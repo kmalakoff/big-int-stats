045af95ef5eb5aa3729cdfd5797ccd05dfc238f2
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 16:53:20 -0800

    Upgrade modules

--- test/lib/polyfills.ts
+++
@@ -1,2 +0,0 @@
-import 'core-js/actual/string/ends-with.js';
-import 'core-js/actual/number/is-safe-integer.js';
--- test/lib/verifyStats.ts
+++ test/lib/verifyStats.ts
@@ -2,6 +2,7 @@ import assert from 'assert';
 import JSBI from 'jsbi-compat';
 import isDate from 'lodash.isdate';
 import { inspect } from 'util';
+import { numberIsSafeInteger, stringEndsWith } from './compat.ts';
 
 const kNsPerMsBigInt = JSBI.BigInt(10 ** 6);
 
@@ -25,7 +26,7 @@ export default function verifyStats(bigintStats, numStats, allowableDelta) {
       assert.strictEqual(bigintStats.isFile(), numStats.isFile());
       assert.strictEqual(bigintStats.isSocket(), numStats.isSocket());
       assert.strictEqual(bigintStats.isSymbolicLink(), numStats.isSymbolicLink());
-    } else if (key.endsWith('Ms')) {
+    } else if (stringEndsWith(key, 'Ms')) {
       var nsKey = key.replace('Ms', 'Ns');
       var msFromBigInt = bigintStats[key];
       var nsFromBigInt = bigintStats[nsKey];
@@ -35,7 +36,7 @@ export default function verifyStats(bigintStats, numStats, allowableDelta) {
       assert(msFromNum - JSBI.toNumber(msFromBigInt) <= allowableDelta, `Number version ${key} = ${msFromNum}, BigInt version ${key} = ${JSBI.toNumber(msFromBigInt)}n, Allowable delta = ${allowableDelta}`);
 
       assert(msFromNum - JSBI.toNumber(msFromBigIntNs) <= allowableDelta, `Number version ${key} = ${msFromNum}, BigInt version ${nsKey} = ${JSBI.toNumber(nsFromBigInt)}n = ${JSBI.toNumber(msFromBigIntNs)}ms, Allowable delta = ${allowableDelta}`);
-    } else if (Number.isSafeInteger(val)) {
+    } else if (numberIsSafeInteger(val)) {
       assert.ok(JSBI.equal(bigintStats[key], JSBI.BigInt(val)), `${inspect(bigintStats[key])} !== ${inspect(JSBI.BigInt(val))}\nkey=${key}, val=${val}`);
     } else {
       typeof bigintStats[key] === 'undefined' || assert(JSBI.toNumber(bigintStats[key]) - val < 1, `${key} is not a safe integer, difference should < 1.\nNumber version ${val}, BigInt version ${bigintStats[key]}n`);
